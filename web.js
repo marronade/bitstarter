cc4f003b761d21811bb9497fecf3bdc1a4ebffc7
Rick Marron <rcmarron@gmail.com>
2013-07-15 04:24:40 +0000

    fixedsemicolon

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(
-      fs.readFileSync("index.html").toString();
-  );    
+      fs.readFileSync("index.html").toString()
+  );
 });
 
 var port = process.env.PORT || 5000;
