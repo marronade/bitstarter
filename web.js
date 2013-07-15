86505f42f4a7255be184bb5e4842ab917fbc7186
Rick Marron <rcmarron@gmail.com>
2013-07-15 04:23:03 +0000

    fixed paren

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(
-  fs.readFileSync("index.html").toString();
-)};
+      fs.readFileSync("index.html").toString();
+  );    
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
